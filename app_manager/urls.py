cb67d81f074a225c24d90810b28296f13aeb7003
Xefex <atorres1929@gmail.com>
2024-08-25 11:48:28 -0400

    Update urls

--- app_manager/urls.py
+++ app_manager/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('start-zomboid/', views.start_zomboid, name='start-zomboid'),
-    path('stop-zomboid/', views.stop_zomboid, name='stop_zomboid'),
-    path('log-zomboid/', views.log_zomboid, name='log-zomboid'),
+    path('api/start-zomboid/', views.start_zomboid, name='start-zomboid'),
+    path('api/stop-zomboid/', views.stop_zomboid, name='stop_zomboid'),
+    path('api/log-zomboid/', views.log_zomboid, name='log-zomboid'),
 ]
